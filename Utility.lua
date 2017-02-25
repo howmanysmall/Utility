8f12af50f3133eb9935d44e600e9e105c02eb2b1
Narrev <NarrevRBX@gmail.com>
2017-02-24 23:36:12 -0600

    Fixed spacing issues

--- Utility.lua
+++ Utility.lua
@@ -6,20 +6,20 @@ local GetChildren = game.GetChildren
 local function GetDescendants(Object)
     --- Like GetChildren, but returns a table with all Descendants
 
-    assert(typeof(Object) == "Instance", "Object is not an Instance")
-    local Descendants = GetChildren(Object)
-    local NumDescendants = #Descendants
-    local Count = 0
+	assert(typeof(Object) == "Instance", "Object is not an Instance")
+	local Descendants = GetChildren(Object)
+	local NumDescendants = #Descendants
+	local Count = 0
 
-    repeat
+	repeat
 		Count = Count + 1
-        local GrandChildren = GetChildren(Descendants[Count])
-        local NumGrandChildren = #GrandChildren
-        for a = 1, NumGrandChildren do
-            Descendants[NumDescendants + a] = GrandChildren[a]
-        end
-    	NumDescendants = NumDescendants + NumGrandChildren
-    until Count == NumDescendants
+		local GrandChildren = GetChildren(Descendants[Count])
+		local NumGrandChildren = #GrandChildren
+		for a = 1, NumGrandChildren do
+			Descendants[NumDescendants + a] = GrandChildren[a]
+		end
+		NumDescendants = NumDescendants + NumGrandChildren
+	until Count == NumDescendants
 
 	return Descendants
 end
@@ -33,16 +33,14 @@ local Key = "CallOnChildren"
 local GetFunction = GetChildren
 for a = 1, 2 do
 	Utility[Key] = function(Object, FunctionToCall)
+		assert(typeof(Object) == "Instance", "Object is not an Instance")
+		assert(type(FunctionToCall) == "function", "FunctionToCall is not a function")
 
-        assert(typeof(Object) == "Instance", "Object is not an Instance")
-        assert(type(FunctionToCall) == "function", "FunctionToCall is not a function")
-
-        local Objects = GetFunction(Object)
-
-        for a = 1, #Objects do
-            FunctionToCall(Objects[a])
-        end
-    end
+		local Objects = GetFunction(Object)
+		for a = 1, #Objects do
+			FunctionToCall(Objects[a])
+		end
+	end
 	Key = "CallOnDescendants"
 	GetFunction = GetDescendants
 end
