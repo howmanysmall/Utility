b6d1d6cbaa5cdd9f452046548e5f14f0c49eaf0e
Validark <NarrevRBX@gmail.com>
2017-06-05 17:56:31 -0500

    Update Utility.lua

--- Utility.lua
+++ Utility.lua
@@ -5,23 +5,26 @@ local GetChildren = game.GetChildren
 
 local function GetDescendants(Object)
     --- Like GetChildren, but returns a table with all Descendants
+	
+	if typeof(Object) == "Instance" then
+		local Descendants = GetChildren(Object)
+		local NumDescendants = #Descendants
+		local Count = 0
 
-	assert(typeof(Object) == "Instance", "Object is not an Instance")
-	local Descendants = GetChildren(Object)
-	local NumDescendants = #Descendants
-	local Count = 0
+		repeat
+			Count = Count + 1
+			local GrandChildren = GetChildren(Descendants[Count])
+			local NumGrandChildren = #GrandChildren
+			for a = 1, NumGrandChildren do
+				Descendants[NumDescendants + a] = GrandChildren[a]
+			end
+			NumDescendants = NumDescendants + NumGrandChildren
+		until Count == NumDescendants
 
-	repeat
-		Count = Count + 1
-		local GrandChildren = GetChildren(Descendants[Count])
-		local NumGrandChildren = #GrandChildren
-		for a = 1, NumGrandChildren do
-			Descendants[NumDescendants + a] = GrandChildren[a]
-		end
-		NumDescendants = NumDescendants + NumGrandChildren
-	until Count == NumDescendants
-
-	return Descendants
+		return Descendants
+	else
+		error("Object is not an Instance")
+	end
 end
 
 local Utility = {
